df6fd2a5439858c8a4d19e6eee5afb0458ca7f6f
thapelomagqazana <tapsmcgzee8@gmail.com>
2023-02-15 16:29:11 +0200

    Non OOP, Bank_Version 1 for Single Account completed

--- bank1_OneAccount.py
+++ bank1_OneAccount.py
@@ -1,1 +1,62 @@
+# Non OOP
+# Bank Version 1
+# Single Account
 
+accountName = 'Joe'
+accountBalance = 100
+accountPassword = 'soup'
+
+while True:
+    print()
+    print('Press b to get the balance')
+    print('Press d to make a deposit')
+    print('Press w to make a withdrawal')
+    print('Press s to show the account')
+    print('Press q to quit')
+    print()
+
+    action = input('What do you want to do? ').lower()
+
+    if action.startswith('b'):
+        print('Get Balance:')
+        userPassword = input('Please enter the password: ')
+        if userPassword != accountPassword:
+            print('Incorrect Password')
+        else:
+            print(f'Your balance is: {accountBalance}')
+    
+    elif action.startswith('d'):
+        print('Deposit:')
+        userDepositAmount = input('Please enter amount to deposit: ')
+        userPassword = input('Please enter the password: ')
+
+        if not userDepositAmount.isdigit():
+            print('Deposit must be a number!')
+        elif int(userDepositAmount) < 0:
+            print('You cannot deposit a negative amount!')
+        elif userPassword != accountPassword:
+            print('Incorrect Password!')
+        else:
+            accountBalance += int(userDepositAmount)
+            print(f'Your new balance is: {accountBalance}')
+
+    elif action.startswith('w'):
+        print('Withdrawal:')
+        userWithdrawalAmount = input('Please enter amount to withdrawal: ')
+        userPassword = input('Please enter the password: ')
+
+        if not userWithdrawalAmount.isdigit():
+            print('Withdrawal must be a number!')
+        elif int(userWithdrawalAmount) < 0:
+            print('You cannot withdraw a negative amount!')
+        elif userPassword != accountPassword:
+            print('Incorrect Password!')
+        elif int(userWithdrawalAmount) > accountBalance:
+            print('You cannot withdraw MORE than the amount in your balance')
+        else:
+            accountBalance -= int(userWithdrawalAmount)
+            print(f'Your new balance is: {accountBalance}')
+
+    elif action.startswith('s'):
+        print(f'Account Name: {accountName}')
+        print(f'Account Password: {accountPassword}')
